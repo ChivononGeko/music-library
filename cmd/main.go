e2e385e7ccb088da49e0ded803801aa658d2943a
Damir <damirdgj@gmail.com>
2024-11-27 15:37:35 +0500

    add: run migrations

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,17 @@ import (
 	"music-library/internal/services"
 	"net/http"
 	"os"
+
+	"github.com/golang-migrate/migrate"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+// @title Music Library API
+// @version 1.0
+// @description This is the API documentation for the Music Library
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
@@ -31,6 +40,13 @@ func main() {
 	defer database.Close()
 	slog.Info("Database connection successfully")
 
+	err = runMigrations(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+	if err != nil {
+		slog.Error("Migrations failed", "error", err)
+		return
+	}
+	slog.Info("Migrations executed successfully")
+
 	repo := repository.NewSongRepository(database, logger)
 	service := services.NewSongService(repo, logger)
 	handler := handlers.NewSongHandler(service, logger)
@@ -42,4 +58,23 @@ func main() {
 		slog.Error("Server failed to start", "error", err)
 		return
 	}
+}
+
+func runMigrations(host, port, user, password, dbname string) error {
+	connString := "postgres://" + user + ":" + password + "@" + host + ":" + port + "/" + dbname + "?sslmode=disable"
+
+	m, err := migrate.New(
+		"file://../migrations",
+		connString,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
 }
