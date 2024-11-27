f017d44b9aed96d892ce9199c6968b0c4fa3b2b8
Damir <damirdgj@gmail.com>
2024-11-27 12:27:45 +0500

    add: script for add table

--- migrations/001_creat_song_table.sql
+++ migrations/001_creat_song_table.sql
@@ -4,5 +4,8 @@ CREATE TABLE songs (
     song_name VARCHAR(255) NOT NULL,
     release_date DATE NOT NULL,
     text TEXT NOT NULL,
-    link VARCHAR(255) NOT NULL
+    link VARCHAR(255) NOT NULL,
+    CONSTRAINT unique_song UNIQUE (group_name, song_name)
 );
+
+CREATE INDEX idx_group_name ON songs(group_name);
