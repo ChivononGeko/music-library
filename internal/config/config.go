5832e9adabce22b9c9635c7ddf9909879fd81c73
Damir <damirdgj@gmail.com>
2024-11-27 11:50:22 +0500

    add: check argumetns

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,17 +18,55 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
+	// Загружаем .env файл
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file, ensure it exists and contains the required values")
 	}
 
+	// Получаем переменные окружения
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		log.Fatal("DB_HOST is not set in environment variables")
+	}
+
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		log.Fatal("DB_PORT is not set in environment variables")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		log.Fatal("DB_USER is not set in environment variables")
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		log.Fatal("DB_PASSWORD is not set in environment variables")
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		log.Fatal("DB_NAME is not set in environment variables")
+	}
+
+	apiPort := os.Getenv("API_PORT")
+	if apiPort == "" {
+		log.Fatal("API_PORT is not set in environment variables")
+	}
+
+	externalAPI := os.Getenv("EXTERNAL_API_URL")
+	if externalAPI == "" {
+		log.Fatal("EXTERNAL_API_URL is not set in environment variables")
+	}
+
+	// Возвращаем конфигурацию
 	return &Config{
-		DBHost:      os.Getenv("DB_HOST"),
-		DBPort:      os.Getenv("DB_PORT"),
-		DBUser:      os.Getenv("DB_USER"),
-		DBPassword:  os.Getenv("DB_PASSWORD"),
-		DBName:      os.Getenv("DB_NAME"),
-		APIPort:     os.Getenv("API_PORT"),
-		ExternalAPI: os.Getenv("EXTERNAL_API_URL"),
+		DBHost:      dbHost,
+		DBPort:      dbPort,
+		DBUser:      dbUser,
+		DBPassword:  dbPassword,
+		DBName:      dbName,
+		APIPort:     apiPort,
+		ExternalAPI: externalAPI,
 	}
 }
